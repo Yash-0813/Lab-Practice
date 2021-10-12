4eddd669604b4fa4bc2f5638c100b1bbcf252057
yashg <yash.gupta2361@gmail.com>
2021-10-12 15:51:26 +0530

    Q1

--- src/test1.java
+++ src/test1.java
@@ -1,3 +1,10 @@
+
+
+
+/*Write a program to create interface named test. In this interface the member
+        function is square. Implement this interface in arithmetic class.
+        Create one new class called ToTestInt in this class use the object of arithmetic class.*/
+
 public interface test1 {
     public int x=8;
     void square();
